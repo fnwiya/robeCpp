1e0dbda4c865be4ecacbc4ba845d2a89afc04cd4
fnwiya <fnwiya@gmail.com>
2016-05-15 22:50:37 +0900

    update

---
+++ 201/File.h
@@ -0,0 +1,38 @@
+/*
+ * File.h
+ *
+ */
+
+#ifndef __SCHEME_FILE__
+#define __SCHEME_FILE__
+
+#include <stdio.h>
+
+class CFile
+{
+ private:
+  FILE* m_pfile;
+  bool m_bCopy;
+ public:
+  CFile();
+  CFile(const CFile& rother);
+  ~CFile();
+ public:
+  bool Open(const char* pszFile, const char* pszFlags);
+  void Close();
+ private:
+  bool ModifyFlags(const char* pszSource, char* pszDest, int nSize);
+ public:
+  size_t Read(void* pData, size_t nSize);
+  size_t Write(const void* pData, size_t  nSize);
+ public:
+  bool IsValid() const;
+  bool Eof();
+};
+
+inline bool CFile::IsValid() const
+{
+  return (m_pfile != NULL);
+}
+
+#endif // __SCHEME_FILE__
---
+++ 201/MainDefs.h
@@ -0,0 +1,11 @@
+/*
+ * MainDefs.h
+ *
+ */
+
+#ifndef __SCHEME_MAINDEFS__
+#define __SCHEME_MAINDEFS__
+
+#define numof(array) (sizeof (array) / sizeof *(array))
+
+#endif // __SCHEME_MAINDEFS__
